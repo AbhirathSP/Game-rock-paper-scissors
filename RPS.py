74d6b31a69b4bf7c91dec8931a41115936464daa
AbhirathSP <abhirathsp2@gmail.com>
2022-07-03 02:28:23 +0530

    Update RPS.py
    Full Game done

--- RPS.py
+++ RPS.py
@@ -1,3 +1,39 @@
 import random
 
 
+youwin = 0
+botwin =0
+
+game = ["r","p","s"]
+while True:
+    userinput = input("Type In\n Rock = r\n Paper = p\n Scissor = s\n Or Quit = q \n ").lower()
+    if userinput == "q":
+        break
+
+    if userinput not in game:
+        continue
+
+    randomnum = random.randint(0,2)
+    botf = game[randomnum]
+    print ("\nBot Choose", botf + ".")
+
+    if userinput == "r" and botf == "s":
+       print("\nYou Won!\n")
+       youwin += 1
+    
+    elif userinput == "p" and botf == "r":
+       print("\nYou Won!\n")
+       youwin += 1
+    
+    elif userinput == "s" and botf == "p":
+       print("\nYou Won!\n")
+       youwin += 1
+    
+    else:
+        print ("\nBot Won :( \n")
+        botwin += 1
+
+print("You won", youwin)
+print("Bot won", botwin)
+print("Thank You!\n")
+
